f4b29833e9e5e77abcc8d26a2217a4e59edcfbe5
zainab perween <zainabperween577@gmail.com>
2026-05-13 22:41:29 +0530

    connecting deployed frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin:"https://kosowa-blwm.vercel.app/",
+  credentials:true,
+}))
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
